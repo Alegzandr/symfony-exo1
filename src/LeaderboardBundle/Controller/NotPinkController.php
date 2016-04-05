914898f46f68fd42bca9f5a89461a685ed275d96
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-04-05 11:52:20 +0200

    Add project

---
+++ src/LeaderboardBundle/Controller/NotPinkController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LeaderboardBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NotPinkController extends Controller
+{
+    public function crimzonCloverAction()
+    {
+        return $this->render('LeaderboardBundle:CrimzonClover:index.html.twig');
+    }
+
+    public function crimzonCloverTypeAction($type)
+    {
+        $type = ['type' => $type];
+        return $this->render('LeaderboardBundle:CrimzonClover:index.html.twig', $type);
+    }
+
+    public function ikarugaAction()
+    {
+        return $this->render('LeaderboardBundle:Ikaruga:index.html.twig');
+    }
+
+    public function ikarugaBgAction($bg)
+    {
+        $bg = ['bg' => $bg];
+        return $this->render('LeaderboardBundle:Ikaruga:index.html.twig', $bg);
+    }
+}
